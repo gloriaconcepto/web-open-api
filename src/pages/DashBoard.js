02398282a952ef594384ac147c9011d015be913b
mmekut-mfon.edet <mmekut-mfon.edet@interswitchgroup.com>
2022-02-19 18:42:45 +0100

    search component created with validation added

--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -3,13 +3,16 @@ import CardDisplay from "../components/CardDisplay";
 import WeatherChart from "../components/WeatherChart";
 import SearchBarComponent from "../utilities/SearchBar";
 const ManageDashBoard = memo((props) => {
+    const getWeatherForcast = (cityName) => {
+        console.log({ cityName });
+    };
     return (
         <section className="col-12">
             <h1 className="weather-header" style={{ marginBottom: "3rem" }}>
                 Weather Forcast App
             </h1>
             <section>
-                <SearchBarComponent />
+                <SearchBarComponent onSearch={getWeatherForcast} />
             </section>
             {/* <section>
                 <CardDisplay />
--- src/utilities/SearchBar.js
+++ src/utilities/SearchBar.js
@@ -1,15 +1,55 @@
 import React, { memo, useEffect, useMemo, useState } from "react";
 
 const SearchBarComponent = (props) => {
+    const [disabled, setDisabled] = useState(true);
+    const [searchWord, setSearchWord] = useState("");
+    const [showErrrorMessage, setShowErrorMessage] = useState(false);
+    const { onSearch } = props;
+    let stringRex = /^[A-Za-z]+$/;
+    const onChange = (e) => {
+        if (e.target.value.length >= 1) {
+            if (!stringRex.test(e.target.value)) {
+                setDisabled(true);
+                setShowErrorMessage(true);
+            } else {
+                setDisabled(false);
+                setShowErrorMessage(false);
+            }
+        } else {
+            setDisabled(false);
+        }
+
+        setSearchWord(e.target.value);
+    };
+    const onBlur = () => {
+        if (searchWord.length === 0) {
+            setDisabled(true);
+        } else {
+            //validate if it contain string only
+            if (!stringRex.test(searchWord)) {
+                setDisabled(true);
+                setShowErrorMessage(true);
+            }
+        }
+    };
+
+    const queryWeather = () => {
+        onSearch(searchWord);
+    };
     return (
         <div className="input-group position-search-bar">
             <div className="form-outline">
-                <small className="form-label" for="form1" style={{ fontStyle: "italic" }}>
+                <small className="form-label" style={{ fontStyle: "italic" }}>
                     Enter city name e.g Tartu
                 </small>
-                <input type="text" id="form1" className="form-control" />
+                <input type="text" id="form1" className="form-control" onBlur={() => onBlur()} onChange={onChange} />
+                {showErrrorMessage && (
+                    <small className="form-label" style={{ fontStyle: "italic", color: "red" }}>
+                        City name must not contain number
+                    </small>
+                )}
             </div>
-            <button type="button" className="btn btn-primary search-bar-button">
+            <button type="button" className="btn btn-primary search-bar-button" disabled={disabled} onClick={queryWeather}>
                 <i className="bi bi-search" style={{ fontSize: 18, color: "white" }}></i>
             </button>
             {/* <p>
